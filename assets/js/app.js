46333b51f1707fd3b8d0fbea38eb69bd9706b36d
Oleg Mironov <olegmrnv@gmail.com>
2019-06-08 14:37:34 -0500

    modified text on circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,80 +1,108 @@
 
 
 
-    var divArea = d3.select(".article");
+var divArea = d3.select(".article");
 
 
-    if (!divArea.empty()) {
-        divArea.remove();
-    }
+if (!divArea.empty()) {
+    divArea.remove();
+}
 
-    var svgArea = d3.select("#scatter").select("svg");
-
-
-    if (!svgArea.empty()) {
-        svgArea.remove();
-    }
-
-    var margin = {
-        top: 50,
-        bottom: 50,
-        right: 50,
-        left: 50
-    };
-
-    var svgWidth = 1000;
-    var svgHeight = 700;
-
-    var plot_height = svgHeight - margin.top - margin.bottom;
-    var plot_width = svgWidth - margin.left - margin.right;
-
-    var svg = d3
-        .select("#scatter")
-        .append("svg")
-        .attr("height", svgHeight)
-        .attr("width", svgWidth);
-
-    var chartGroup = svg.append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var svgArea = d3.select("#scatter").select("svg");
 
 
-    d3.csv("assets/data/data.csv").then(function (stateData) {
-        stateData.forEach(function (data) {
-            data.poverty = +data.poverty;
-            data.smokes = +data.smokes;
-        });
+if (!svgArea.empty()) {
+    svgArea.remove();
+}
 
-        var xScale = d3.scaleLinear()
-            .domain([d3.min(stateData, d => d.smokes) - 1, d3.max(stateData, d => d.smokes) + 1])
-            .range([0, plot_width]);
-        var yScale = d3.scaleLinear()
-            .domain([d3.min(stateData, d => d.poverty) - 1, d3.max(stateData, d => d.poverty) + 1])
-            .range([plot_height, 0]);
+var margin = {
+    top: 50,
+    bottom: 50,
+    right: 50,
+    left: 50
+};
 
-        var xAxis = d3.axisBottom(xScale).ticks(20);
-        var yAxis = d3.axisLeft(yScale).ticks(20);
+var svgWidth = 1000;
+var svgHeight = 700;
 
-        chartGroup.append("g").call(yAxis);
-        chartGroup.append("g").attr("transform", `translate(0, ${plot_height})`).call(xAxis)
+var plot_height = svgHeight - margin.top - margin.bottom;
+var plot_width = svgWidth - margin.left - margin.right;
 
-        var circlesGroup = chartGroup.selectAll("circle")
-            .data(stateData)
-            .enter()
-            .append("circle")
-            .classed("stateCircle", true)
-            .attr("cx", d => xScale(d.smokes))
-            .attr("cy", d => yScale(d.poverty))
-            .attr("r", "15")
-            .append("text")
-            .attr("text", d => d.abbr);
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
-        svg.append("text")            
-            .attr("transform", `translate(${plot_width / 2}, ${plot_height + margin.top + 20})`)
-            .attr("text-anchor", "middle")
-            .attr("font-size", "26px")
-            .attr("fill", "black")
-            .text("Smokes");
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-    })
+d3.csv("assets/data/data.csv").then(function (stateData) {
+    stateData.forEach(function (data) {
+        data.poverty = +data.poverty;
+        data.smokes = +data.smokes;
+    });
+
+    var xScale = d3.scaleLinear()
+        .domain([d3.min(stateData, d => d.smokes) - 1, d3.max(stateData, d => d.smokes) + 1])
+        .range([0, plot_width]);
+    var yScale = d3.scaleLinear()
+        .domain([d3.min(stateData, d => d.poverty) - 1, d3.max(stateData, d => d.poverty) + 1])
+        .range([plot_height, 0]);
+
+    var xAxis = d3.axisBottom(xScale).ticks(20);
+    var yAxis = d3.axisLeft(yScale).ticks(20);
+
+    chartGroup.append("g").call(yAxis);
+    chartGroup.append("g").attr("transform", `translate(0, ${plot_height})`).call(xAxis)
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .classed("stateCircle", true)
+        .attr("cx", d => xScale(d.smokes))
+        .attr("cy", d => yScale(d.poverty))
+        .attr("r", "15");
+
+    // svg
+    //     .data(stateData)
+    //     .enter()
+    //     .append("text")
+    //     .attr("cx", d => xScale(d.smokes))
+    //     .attr("cy", d => yScale(d.poverty))
+    //     .attr("html", d => d.abbr);
+
+    // var textGroup = chartGroup.selectAll()
+    //     .data(stateData)
+    //     .enter()
+    //     .append("text")
+    //     .attr("transform", `translate(${xScale(d.smokes)}, ${yScale(d.poverty)})`)
+    // .attr("cx", d => xScale(d.smokes))
+    // .attr("cy", d => yScale(d.poverty))
+    // .attr("html", d => d.abbr);
+
+    var tickGroup = chartGroup.selectAll()
+        .data(stateData)
+        .enter()        
+        .append("text")
+        // .attr("transform", `translate(${plot_width / 2}, ${plot_height + margin.top + 10})`)
+        // .attr("text-anchor", "middle")
+        // .attr("font-size", "26px")
+        // .attr("fill", "black")
+        .attr("x", d => (xScale(d.smokes))-10)
+        .attr("y", d => (yScale(d.poverty))+6)
+        .attr("font-size", "14px")
+        .text(d => d.abbr);
+
+    svg.append("text")
+        .attr("transform", `translate(${plot_width / 2}, ${plot_height + margin.top + 40})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "26px")
+        .attr("fill", "black")
+        .text("Smokes");
+
+
+})
 
